bd64ef89fb7c2521e40d5b9fff9669f6e9554b5c
VikasDewangan <dewanganvikas192@gmail.com>
2024-03-12 00:52:13 +0530

    database ready

--- app.js
+++ app.js
@@ -1,81 +1,81 @@
 require('dotenv').config()
-// const app = require('express')();
-// const http = require('http').Server(app);
+const app = require('express')();
+const http = require('http').Server(app);
 
-// const mongoose= require('mongoose');
-// const bodyParser = require("body-parser");
+const mongoose= require('mongoose');
+const bodyParser = require("body-parser");
 
-// app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
-// mongoose.connect("mongodb+srv://dewanganvikas192:SBv2odEfcte2gObt@ewaste-db.nft0ncb.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://dewanganvikas192:SBv2odEfcte2gObt@ewaste-db.nft0ncb.mongodb.net/?retryWrites=true&w=majority");
 
-// //create a data schema
-// const infoSchema ={
-//     name:String,
-//     pincode:String,
-//     mobilenumber :String,
-//     No_Of_Product:String,
-//     dateOfPurchase:String,
-//     dateOfPickup:String,
-//     Address : String,
-// }
+//create a data schema
+const infoSchema ={
+    name:String,
+    pincode:String,
+    mobilenumber :String,
+    No_Of_Product:String,
+    dateOfPurchase:String,
+    dateOfPickup:String,
+    Address : String,
+}
  
-// const user  = mongoose.model("info",infoSchema);
+const user  = mongoose.model("info",infoSchema);
 
-// app.get("/",function(req,res){
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+// app.get("/thankyou",function(req,res){
 //     res.sendFile(__dirname+"/index.html");
 // })
-
-// // app.get("/thankyou",function(req,res){
-// //     res.sendFile(__dirname+"/index.html");
-// // })
-// app.post("/",function(req,res){
+app.post("/",function(req,res){
     
-//     // console.log(req.body.name)
-//     // let newUser = new user({
-//     //     name: req.body.name,
-//     //     pincode:req.body.pincode
+    // console.log(req.body.name)
+    // let newUser = new user({
+    //     name: req.body.name,
+    //     pincode:req.body.pincode
 
-//     // });
+    // });
         
-//     //  newUser.save();
+    //  newUser.save();
     
     
     
     
-//     // const user = require('./models/usermodel')
+    // const user = require('./models/usermodel')
     
-//     async function insert(){
-//         user.create({
-//             name:req.body.name,
-//             mobilenumber : req.body.mobileNumber,
-//             No_Of_Product:req.body.No_Of_Product,
-//             dateOfPurchase:req.body.dop,
-//             dateOfPickup: req.body.pickup,
-//             Address : req.body.address,
-//             pincode:req.body.pincode,
+    async function insert(){
+        user.create({
+            name:req.body.name,
+            mobilenumber : req.body.mobileNumber,
+            No_Of_Product:req.body.No_Of_Product,
+            dateOfPurchase:req.body.dop,
+            dateOfPickup: req.body.pickup,
+            Address : req.body.address,
+            pincode:req.body.pincode,
             
-//         })
+        })
         
-//     }
-//     insert();
-//      res.redirect('/');
-//  })
+    }
+    insert();
+     res.redirect('/');
+ })
              
-// http.listen(3000,function(){
-//     console.log("server is running");
-// });
+http.listen(3000,function(){
+    console.log("server is running");
+});
 
 // console.log("hello World");
 
-const express = require('express');
-const app = express();
-const port = 3000
+// const express = require('express');
+// const app = express();
+// const port = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
 
-app.listen(process.env.PORT,() => {
-    console.log("app is listening at port 3000")
-});
+// app.listen(process.env.PORT,() => {
+//     console.log("app is listening at port 3000")
+// });
